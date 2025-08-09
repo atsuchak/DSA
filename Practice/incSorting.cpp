2d60d9a7d72d502031fb764e9c7ae0665e598c22
atsuchak <suchak0630@gmail.com>
2025-08-09 21:55:02 +0600

    add counting sort algorithm

--- Practice/incSorting.cpp
+++ Practice/incSorting.cpp
@@ -9,28 +9,63 @@ void bubble_sort(vector<int> &a, int n)
                 swap(a[j], a[j + 1]);
 }
 
-void selection_sort(vector<int> &a, int n) {
-    for(int i = 0; i < n-1; i++) {
+void selection_sort(vector<int> &a, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
         int sEl = i;
-        for(int j = i+1; j < n; j++) {
-            if(a[j] < a[sEl]) sEl = j;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[sEl])
+                sEl = j;
         }
         swap(a[i], a[sEl]);
     }
 }
 
-void insertion_sort(vector<int> &a, int n) {
-    for(int i = 1; i < n; i++) {
+void insertion_sort(vector<int> &a, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
         int key = a[i];
-        int prev = i-1;
+        int prev = i - 1;
 
-        while(prev >= 0 && a[prev] > key) {
-            a[prev+1] = a[prev];
+        while (prev >= 0 && a[prev] > key)
+        {
+            a[prev + 1] = a[prev];
             prev--;
         }
 
-        a[prev+1] = key;
+        a[prev + 1] = key;
     }
+}
+
+void countingSort(vector<int> &a, int n)
+{
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++){
+        if (a[i] > max)
+            max = a[i];
+    }
+        
+
+    vector<int> store(max+1, 0);
+
+    for (int i = 0; i < n; i++)
+        store[a[i]]++;
+
+    for(int i = 1; i <= max; i++) store[i] += store[i-1];
+
+    vector<int> result(n);
+
+    for(int i = n-1; i >=0; i--) {
+        result[--store[a[i]]] = a[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        a[i] = result[i];
+    }
+
 }
 
 void print(vector<int> &a, int n)
@@ -43,12 +78,13 @@ void print(vector<int> &a, int n)
 int main()
 {
 
-    int n = 5;
-    vector<int> a = {4, 1, 5, 2, 3};
+    int n = 7;
+    vector<int> a = {4, 1, 2, 5, 2, 3, 0};
 
     // bubble_sort(a, n);
     // selection_sort(a, n);
-    insertion_sort(a, n);
+    // insertion_sort(a, n);
+    countingSort(a, n);
 
     print(a, n);
 
